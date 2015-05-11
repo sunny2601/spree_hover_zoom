99c10735a008a0fcb70669147e1bf2c6fbc001ac
sunny2601 <sunnysarrot@gmail.com>
2015-05-11 11:33:42 +0530

    Update install_generator.rb

--- lib/generators/spree_hover_zoom/install/install_generator.rb
+++ lib/generators/spree_hover_zoom/install/install_generator.rb
@@ -3,11 +3,11 @@ module SpreeHoverZoom
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/spree/frontend/all.js', "//= require frontend/easyzoom.js\n"
+        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require frontend/easyzoom.js\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/spree/frontend/all.css', " *= require frontend/spree_hover_zoom\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require frontend/spree_hover_zoom\n", :before => /\*\//, :verbose => true
       end
 
       #def add_migrations
